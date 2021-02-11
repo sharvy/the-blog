5086df8320bed08573b8be314ab3a664345c73e4
Sharvy Ahmed <sharvy2008@gmail.com>
2021-02-11 23:42:33 +0600

    Added Custom Hook - useFetch to fetch external data.

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,15 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "Blog 1", body: "lorem ipsum...", author: "mario 1", id: 1 },
-    { title: "Blog 2", body: "lorem ipsum...", author: "mario 2", id: 2 },
-    { title: "Blog 3", body: "lorem ipsum...", author: "mario 3", id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    console.log("Use Effect Working...")
-  }, [])
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
