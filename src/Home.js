cf5e7411c8b34ef80563daa137ecf3e1e896b8f4
Sharvy Ahmed <sharvy2008@gmail.com>
2021-02-11 21:16:29 +0600

    Added some styling and json-server.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "Blog 1", body: "lorem ipsum...", author: "mario 1", id: 1 },
+    { title: "Blog 2", body: "lorem ipsum...", author: "mario 2", id: 2 },
+    { title: "Blog 3", body: "lorem ipsum...", author: "mario 3", id: 3 },
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("Use Effect Working...")
+  }, [])
+
   return (
     <div className="home">
-      Home Page
+      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
